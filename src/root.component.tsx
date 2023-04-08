7c229815a8883b8dcf7b9c4392b141baa0967334
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-08 18:24:14 -0300

    feat: setup initial communication with osiris-ui

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,24 @@
+// @ts-nocheck
+import {
+  DefaultLayout,
+  Flex,
+  Logo,
+  Center,
+  TextBadge,
+} from "@stagepass/osiris-ui";
+
+import LogoImg from "./assets/logo.png";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <DefaultLayout>
+      <Center h="90vh">
+        <Logo src={LogoImg} borderRadius="60px" boxSize="320px" />
+      </Center>
+
+      <Flex gap="48" alignItems="center" justify="center" mt="20">
+        <TextBadge color="secondary">concertsaasdfasdfas</TextBadge>
+      </Flex>
+    </DefaultLayout>
+  );
 }
