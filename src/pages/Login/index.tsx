08d7764902daf530f3eba3081b9de896c4e3aae3
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-11 09:41:47 -0300

    feat: creates useAuth hook for supabase login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,52 +1,10 @@
-import { Auth } from "@supabase/auth-ui-react"
-import { createClient } from "@supabase/supabase-js"
-import { useEffect, useState } from "react"
-import {
-  // Import predefined theme
-  ThemeSupa,
-} from '@supabase/auth-ui-shared'
-import { Box } from "@stagepass/osiris-ui"
-
-
-const supabase = createClient('https://mintqpocupbvbhaqlmtu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pbnRxcG9jdXBidmJoYXFsbXR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODExNzU0NjYsImV4cCI6MTk5Njc1MTQ2Nn0.a5VbtP5aoRzcTfk8pMfMDexWyiVvW4ja45d_SGdXTCM')
+import { useAuth } from "../../services/auth"
 
 export const Login = () => {
-  const [session, setSession] = useState(null)
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({data: {session}}) => {
-      setSession(session)
-    })
-
-    const {
-      data: {subscription},
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-    
-    return () => subscription.unsubscribe()
-  }, [])
+  const {session, getAuthUI} = useAuth()
 
   if (!session) {
-    return (
-      <Box style={{
-        height: '100vh',
-        width: '100vw',
-        display: 'flex',
-
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#ffffff'
-      }}>
-        <div style={{width: '40%'}}>
-        <Auth  
-          supabaseClient={supabase} 
-          appearance={{theme: ThemeSupa}}
-          providers={['google', 'apple']}
-        />
-        </div>
-      </Box>
-    )
+    return getAuthUI()
   }
   
   return (
