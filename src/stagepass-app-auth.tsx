3b68cd23cfe9f0080d5b3b64b6855a6800a7bff8
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-16 03:23:39 -0300

    chore: add sentry to project

--- src/stagepass-app-auth.tsx
+++ src/stagepass-app-auth.tsx
@@ -2,27 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 
-// import * as Sentry from "@sentry/browser";
-// import { Integrations } from "@sentry/tracing";
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
 import Root from "./root.component";
 
-// Change to env variable later on
-// Sentry.init({
-//   dsn: "INSERT_DSN_HERE",
-//   integrations: [new Integrations.BrowserTracing()],
-//   tracesSampleRate: 1.0,
-// });
+Sentry.init({
+  dsn: "https://a5fe265f8716489bb751c5a172f55385@o4504899977936896.ingest.sentry.io/4505022371528704",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: Root,
   errorBoundary(err, info, props) {
-    // Sentry.withScope((scope) => {
-    //   scope.setExtra("props", props);
-    //   scope.setExtra("info", info);
-    //   Sentry.captureException(err);
-    // });
+    Sentry.withScope((scope) => {
+      scope.setExtra("props", props);
+      scope.setExtra("info", info);
+      Sentry.captureException(err);
+    });
 
     return null;
   },
