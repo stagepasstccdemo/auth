7361047d638a67b3e652e74803867cd28197aacb
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-16 14:45:24 -0300

    refactor: page using new osiris-ui components

--- src/pages/Login/steps/first_access/ShowHowSecond.tsx
+++ src/pages/Login/steps/first_access/ShowHowSecond.tsx
@@ -3,12 +3,31 @@ import {
   Box,
   DefaultLayout,
   Flex,
-  Pills,
   Heading,
-  TextBadge,
+  MultipleBadges,
+  FullFlexWithGestures,
+  MultiplePills,
 } from "@stagepass/osiris-ui";
 
 import { useSwipeGestures } from "@hooks/useSwipeable";
+
+const badges = [
+  { key: 1, text: "concerts", bgColor: "default" },
+  { key: 2, text: "festivals", bgColor: "primary", width: "60%" },
+  {
+    key: 3,
+    text: "sports",
+    bgColor: "ternary",
+    width: "80%",
+  },
+  { key: 4, text: "talks", bgColor: "secondary", width: "100%" },
+];
+
+const pills = [
+  { key: 1, selected: false },
+  { key: 2, selected: true },
+  { key: 3, selected: false, onClick: () => setPage(page + 1) },
+];
 
 export function ShowHowSecond({ setPage, page }) {
   const { swipeGestures } = useSwipeGestures({
@@ -21,29 +40,22 @@ export function ShowHowSecond({ setPage, page }) {
 
   return (
     <DefaultLayout>
-      <Flex
-        flexDirection="column"
-        alignItems="center"
+      <FullFlexWithGestures
         justifyContent="space-between"
-        height="100vh"
-        {...swipeGestures}
+        swipe={swipeGestures}
       >
         <Box px="40" py="52">
           <Heading as="h1" color="gray.100">
             Have Fun In every way possible
           </Heading>
-          <Flex direction="column" alignItems="start" pt="20" gap="20">
-            <TextBadge bgColor="default">concerts</TextBadge>
-            <TextBadge width="60%" bgColor="primary">
-              festivals
-            </TextBadge>
-            <TextBadge width="80%" bgColor="ternary" variant="outline">
-              sports
-            </TextBadge>
-            <TextBadge width="100%" bgColor="secondary">
-              talks
-            </TextBadge>
-          </Flex>
+
+          <MultipleBadges
+            direction="column"
+            alignItems="start"
+            pt="20"
+            gap="20"
+            badges={badges}
+          />
           <Flex alignItems="center" pt="40" pl="20" textAlign="flex-start">
             <Heading as="h2" color="gray.100">
               and a whole lot more...
@@ -51,12 +63,8 @@ export function ShowHowSecond({ setPage, page }) {
           </Flex>
         </Box>
 
-        <Flex gap={20} pb="22">
-          <Pills />
-          <Pills selected onClick={() => setPage(page + 1)} />
-          <Pills />
-        </Flex>
-      </Flex>
+        <MultiplePills gap={20} pb={22} alignItems="center" pills={pills} />
+      </FullFlexWithGestures>
     </DefaultLayout>
   );
 }
