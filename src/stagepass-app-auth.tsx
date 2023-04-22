b2978174da5228d851b9f7045093c1834d90aa00
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-22 18:22:58 -0300

    chore: make sentry ignore localhost

--- src/stagepass-app-auth.tsx
+++ src/stagepass-app-auth.tsx
@@ -10,6 +10,12 @@ Sentry.init({
   dsn: "https://a5fe265f8716489bb751c5a172f55385@o4504899977936896.ingest.sentry.io/4505022371528704",
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 1.0,
+  beforeSend: (event) => {
+    if (window.location.hostname === "localhost") {
+      return null;
+    }
+    return event;
+  },
 });
 
 const lifecycles = singleSpaReact({
